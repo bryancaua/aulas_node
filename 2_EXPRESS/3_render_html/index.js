ba48643d0d847319bf928c9a30a4dd9eb3f46b84
bryan <bryanncaua@gmail.com>
2023-10-02 13:28:30 -0300

    aula de vídeo

---
+++ 2_EXPRESS/3_render_html/index.js
@@ -0,0 +1,13 @@
+const { request } = require("express")
+const express = require("express")
+
+const app = express()
+
+app.get('/', (requisicao, resposta) => {
+    resposta.send("Estou funcionando!")
+})
+
+app.listen(3000, () => {
+    console.log("Servidor rodando na porta 3000")
+})
+
